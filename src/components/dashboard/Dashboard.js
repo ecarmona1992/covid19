5440a4f44373ea327a273dbac3238f062b4e9eb2
Emanuel Carmona <emanuel@Emanuels-MacBook-Pro.local>
2020-05-11 13:51:02 -0700

    finished adding post and user

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,21 +4,21 @@ import Notifications from './Notifications'
 import { connect } from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import { Redirect } from 'react-router-dom'
+
 
 class Dashboard extends Component {
   render() {
     
-    // console.log(this.props);
-    const { projects } = this.props;
+   // console.log(this.props);
+    const { projects,auth } = this.props;
+    if (!auth.uid) return <Redirect to='/signin'/>
     
     return (
-      <div className="dashboard container">
+      <div className="dashboard container valign-wrapper">
         <div className="row">
-          <div className="col s12 m6">
+          <div className="col">
             <ProjectList projects={projects} />
-          </div>
-          <div className="col s12 m5 offset-m1">
-            <Notifications />
           </div>
         </div>
       </div>
@@ -27,9 +27,10 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = (state) => {
-  // console.log(state);
+  //console.log(state);
   return {
-    projects: state.firestore.ordered.projects
+    projects: state.firestore.ordered.projects,
+    auth: state.firebase.auth
   }
 }
 
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import ProjectSummary from './ProjectSummary'
 import { Link } from 'react-router-dom'
