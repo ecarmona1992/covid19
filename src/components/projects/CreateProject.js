a0f4f398d332af8439e73a929ce970748172a57b
Emanuel Carmona <emanuel@Emanuels-MacBook-Pro.local>
2020-05-16 14:27:09 -0700

    changed text

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -27,14 +27,14 @@ class CreateProject extends Component {
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
-          <h5 className="grey-text text-darken-3">Create a New Project</h5>
+          <h5 className="grey-text text-darken-3">Create a New Post</h5>
           <div className="input-field">
             <input type="text" id='title' onChange={this.handleChange} />
-            <label htmlFor="title">Project Title</label>
+            <label htmlFor="title">Post Title</label>
           </div>
           <div className="input-field">
             <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
-            <label htmlFor="content">Project Content</label>
+            <label htmlFor="content">Post Content</label>
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1">Create</button>
